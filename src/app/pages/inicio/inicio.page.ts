cc6ada1c8d08c67011f54b2dfc26590546270072
Jesus Serrano <jsf031994@gmail.com>
2021-11-26 13:59:19 +0100

    Fin paso 47

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+
+}
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [{
+    icon: 'american-football-outline',
+    name: 'Action sheet',
+    redirecTo: '/action-sheet'
+  },
+  {
+    icon: 'alert-circle-outline',
+    name: 'Alert',
+    redirecTo: '/alert'
+  },
+  {
+    icon: 'beaker-outline',
+    name: 'Avatar',
+    redirecTo: '/avatar'
+  },
+  {
+    icon: 'radio-button-off-outline',
+    name: 'Button',
+    redirecTo: '/button'
+  }
+];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
